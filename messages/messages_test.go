03d61f7b64e730711e76bedff4ae593184992bb5
Léonard Jouve <leonard.jouve@gmail.com>
2025-02-07 20:57:58 +0100

    move utils to messages

--- messages/messages_test.go
+++ messages/messages_test.go
@@ -1,15 +1,57 @@
-package utils_test
+package messages_test
 
 import (
+	"bytes"
 	"dofus-sniffer/messages"
 	"dofus-sniffer/proto/game/game_message"
-	"dofus-sniffer/utils"
 	"encoding/hex"
+	"fmt"
 	"reflect"
 	"testing"
 
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/types/known/anypb"
 )
+
+func isOfType(message proto.Message, messageType protoreflect.MessageType) (bool, bool) {
+	gameMessage, ok := message.(*game_message.Message)
+	if !ok {
+		return false, false
+	}
+
+	var any *anypb.Any
+	switch gameMessage.Content.(type) {
+	case *game_message.Message_Request:
+		any = gameMessage.GetRequest().GetContent()
+	case *game_message.Message_Event:
+		any = gameMessage.GetEvent().GetContent()
+	case *game_message.Message_Response:
+		any = gameMessage.GetResponse().GetContent()
+	default:
+		return false, false
+	}
+
+	msg := messageType.New().Interface()
+	err := proto.Unmarshal(any.GetValue(), msg)
+	if err != nil {
+		return false, false
+	}
+
+	json, err := protojson.MarshalOptions{Indent: "  "}.Marshal(msg)
+	if err != nil {
+		return false, false
+	}
+	fmt.Printf("%s\n%s\n", any.GetTypeUrl(), string(json))
+
+	buffer, err := proto.Marshal(msg)
+	if err != nil {
+		return false, false
+	}
+
+	return bytes.Equal(any.GetValue(), buffer), true
+}
 
 func testMessage(t *testing.T, hexPayload string, messageType string) {
 	payload, err := hex.DecodeString(hexPayload)
@@ -33,7 +75,7 @@ func testMessage(t *testing.T, hexPayload string, messageType string) {
 		t.Fatalf("Could not find type\n")
 	}
 
-	isOfType, ok := utils.IsOfType(message, protoType)
+	isOfType, ok := isOfType(message, protoType)
 	if !ok {
 		t.Fatalf("Could not execute IsOfType\n")
 	}
